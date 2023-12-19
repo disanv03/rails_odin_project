6972f61e29d0ae00171ef65400ca6ec30a79be00
disanv03 <heddisanv@gmail.com>
2023-12-19 12:31:19 +0100

    Routing

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+#   get "/posts", to: "posts#index"
+#   get "/posts/new", to: "posts#new"
+#   get "/posts/:id", to: "posts#show"
+#   post "/posts", to: "posts#create"  # usually a submitted form
+#   get "/posts/:id/edit", to: "posts#edit"
+#   put "/posts/:id", to: "posts#update" # usually a submitted form
+#   delete "/posts/:id", to: "posts#destroy"
+
+  #   equivalent:
+  #   resources :posts
+
+  # simple, non-restful routes
+  # get '/somepath', to: 'somecontroller#someaction'
 end
